acaff13a3ae619939f32fdc0077bd1b7d8781726
Lucas Ferreira <lucas.182.ferreira@hotmail.com>
2025-01-24 16:46:23 -0300

    Adjusting sanitization and adjusting font-size

--- src/utils/sanitize.ts
+++ src/utils/sanitize.ts
@@ -1,6 +1,6 @@
 export const sanitizeBusinessName = (name: string): string => {
     return name
-        .replace(/^\s*"+|"+\s*$/g, '')
+        .replace(/^[\s"%&#]+|[\s"%&#]+$/g, '')
         .replace(/\s*,\s*$/, '')
         .replace(/\s{2,}/g, ' ')
       .trim();
