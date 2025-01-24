552a45365ad5cc55c197b14b35012e987df5a73e
Lucas Ferreira <lucas.182.ferreira@hotmail.com>
2025-01-24 17:34:53 -0300

    Bringing logo back

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -4,6 +4,7 @@ import styles from './styles.module.css';
 
 const HomePage = () => (
   <div className={styles.container} data-testid='home-page'>
+    <header className={styles.logo} />
     <h1 className={styles.title}>Food Hygiene Rating Dashboard</h1>
     <PaginatedEstablishmentsTable />
   </div>
